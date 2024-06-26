95ebcc3e4e23345dacb16fa75f7a5808ed06da26
YvesKingC <ychandra02@student.ciputra.ac.id>
2024-06-26 10:15:28 +0700

    mulai

---
+++ Waterpedia Winform/LoginPage.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Waterpedia_Winform
+{
+    partial class LoginPage
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblEmail = new Label();
+            lblPassword = new Label();
+            SuspendLayout();
+            // 
+            // lblEmail
+            // 
+            lblEmail.AutoSize = true;
+            lblEmail.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            lblEmail.Location = new Point(65, 143);
+            lblEmail.Name = "lblEmail";
+            lblEmail.Size = new Size(259, 45);
+            lblEmail.TabIndex = 0;
+            lblEmail.Text = "Username/Emaill";
+            lblEmail.Click += lblEmail_Click;
+            // 
+            // lblPassword
+            // 
+            lblPassword.AutoSize = true;
+            lblPassword.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            lblPassword.Location = new Point(65, 212);
+            lblPassword.Name = "lblPassword";
+            lblPassword.Size = new Size(153, 45);
+            lblPassword.TabIndex = 1;
+            lblPassword.Text = "Password";
+            // 
+            // LoginPage
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblPassword);
+            Controls.Add(lblEmail);
+            Name = "LoginPage";
+            Text = "Login Page";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblEmail;
+        private Label lblPassword;
+    }
+}
---
+++ Waterpedia Winform/LoginPage.cs
@@ -0,0 +1,15 @@
+namespace Waterpedia_Winform
+{
+    public partial class LoginPage : Form
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void lblEmail_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
