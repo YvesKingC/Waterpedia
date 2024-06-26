4930a1701ae80583f71ecc1333c9536ce860cf68
Okky Julian Atmajaya Tarmoko <oatmajaya@student.ciputra.ac.id>
2024-06-26 11:48:31 +0700

    ui ril

--- Waterpedia Winform/obj/Release/net6.0-windows/Waterpedia Winform.AssemblyInfo.cs
+++ Waterpedia Winform/obj/Release/net6.0-windows/Waterpedia Winform.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Waterpedia Winform")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+462839edbd94d5dd646317964cb07291d66119f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Waterpedia Winform")]
